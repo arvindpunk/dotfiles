1a53ed71049490dc345f9063620f45dd4820b508
Arvind PJ <arvindpunk@gmail.com>
2025-06-08 16:41:11 +0530

    chore: remove ghostty

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -30,14 +30,9 @@
     obsidian
     scrot
     awscli2
+    redis
   ];
 
-
-  programs = {
-    ghostty = {
-      enable = true;
-    };
-  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
